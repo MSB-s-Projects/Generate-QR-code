c9feeb947c959898ce87aef48841867ce5a19a15
Manobal Singh Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2023-03-04 05:03:31 +0530

    Update app.js
    Co-Authored-By: Ujjawal Gupta <113056503+ugpec79@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+var createError = require('http-errors')
 var express = require('express')
 var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
+
 var indexRouter = require('./routes/index')
 var usersRouter = require('./routes/users')
 var bodyParser = require('body-parser')
@@ -11,6 +13,7 @@ var app = express()
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(logger('dev'))
 app.use(express.json())
@@ -20,6 +23,11 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+	next(createError(404))
+})
 
 // error handler
 app.use(function (err, req, res, next) {
