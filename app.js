b0ab2ab7b87c13ccf6ed1200bbcac61595512561
Manobal Singh Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2023-03-04 03:40:37 +0530

    move index route to index.js
    Move the index.js route requests to index.js file.
    
    Co-Authored-By: Ujjawal Gupta <113056503+ugpec79@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,28 +1,16 @@
-var createError = require('http-errors')
 var express = require('express')
 var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
-const { v4: uuidV4 } = require('uuid')
-const QRcode = require('qrcode')
 var indexRouter = require('./routes/index')
 var usersRouter = require('./routes/users')
-const bodyParser = require('body-parser')
-var mongoose = require('mongoose')
-mongoose.connect(process.env.MONGO + '/QRDB', { useNewUrlParser: true })
-// QRcode.toDataURL()
-const urlSchema = mongoose.Schema({
-	name: String,
-	url: String,
-})
-const URL = new mongoose.model('url', urlSchema)
+var bodyParser = require('body-parser')
 
 var app = express()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(logger('dev'))
 app.use(express.json())
@@ -30,83 +18,8 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-// app.use("/", indexRouter);
-app.get('/', function (req, res) {
-	res.render('index')
-})
-app.post('/', function (req, res) {
-	console.log(req.body)
-	if (req.body.Text) {
-		QRcode.toDataURL(req.body.Text, (err, url) => {
-			if (!err) {
-				// rendering the qr code generated
-				console.log(url)
-				res.render('qrcode', { url: url })
-			} else {
-				console.log(err)
-			}
-		})
-	} else if (req.body['URL']) {
-		QRcode.toDataURL(req.body['URL'], (err, url) => {
-			if (!err) {
-				// rendering the qr code generated
-				console.log(url)
-				res.render('qrcode', { url: url })
-			} else {
-				console.log(err)
-			}
-		})
-	} else {
-		var newVar = uuidV4()
-		var base64Str = req.body.ImageString
-		URL.findOne({ name: newVar })
-			.then((url) => {
-				if (err) {
-					console.log(err)
-					res.send('Error')
-				} else {
-					res.send('Already Exist')
-				}
-			})
-			.catch((error) => {
-				const ur = new URL({
-					name: newVar,
-					url: base64Str,
-				})
-				ur.save()
-				QRcode.toDataURL(process.env.URL + `/image/${newVar}`, (er, ul) => {
-					if (!er) {
-						console.log(ul)
-						// rendering the qr code generated
-						res.render('qrcode', { url: ul })
-					} else {
-						console.log(er)
-					}
-				})
-			})
-	}
-})
+app.use('/', indexRouter)
 app.use('/users', usersRouter)
-app.get('/image/:test', function (req, res) {
-	console.log(req.params.test)
-	URL.findOne({ name: req.params.test })
-		.then((rl) => {
-			console.log(rl)
-			res.render('image', { img: rl.url })
-		})
-		.catch((error) => {
-			res.send('Not Found')
-		})
-
-	// var base64Str=req.query.src;
-	// console.log(base64Str);
-	// res.render("image",{img:base64Str});
-})
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-	next(createError(404))
-})
 
 // error handler
 app.use(function (err, req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,30 @@
 // importing express
 var express = require('express')
-
+// import dotenv and configure it
 require('dotenv').config()
-var express = require('express')
 // importing qrcode
 const QRcode = require('qrcode')
+// importing uuid
+const { v4: uuidV4 } = require('uuid')
+// import mongoose
+var mongoose = require('mongoose')
+
+// connecting to the database
+mongoose.connect(process.env.MONGO + '/QRDB', { useNewUrlParser: true })
+
+// create a URL schema
+const urlSchema = mongoose.Schema({
+	name: String,
+	url: String,
+})
+
+// create a URL model
+const URL = new mongoose.model('url', urlSchema)
 
 // creating express router
 var router = express.Router()
-const { v4: uuidV4 } = require('uuid')
+
+// creating the root route
 router
 	.route('/')
 
@@ -20,31 +36,35 @@ router
 
 	// post request
 	.post((req, res) => {
-		// create a qr code from the form data
-		console.log(req.body)
+		// if the request body has a text field
 		if (req.body.Text) {
+			// generating a qr code from the text
 			QRcode.toDataURL(req.body.Text, (err, url) => {
 				if (!err) {
 					// rendering the qr code generated
-					console.log(url)
 					res.render('qrcode', { url: url })
 				} else {
 					console.log(err)
 				}
 			})
+			// if the request body has a url field
 		} else if (req.body['URL']) {
+			// generating a qr code from the url
 			QRcode.toDataURL(req.body['URL'], (err, url) => {
 				if (!err) {
 					// rendering the qr code generated
-					console.log(url)
 					res.render('qrcode', { url: url })
 				} else {
 					console.log(err)
 				}
 			})
+			// if the request body has a image field
 		} else {
+			// generating a random string
 			var newVar = uuidV4()
+			// getting the base64 string of the image
 			var base64Str = req.body.ImageString
+			// checking if the image already exists
 			URL.findOne({ name: newVar })
 				.then((url) => {
 					if (err) {
@@ -54,15 +74,18 @@ router
 						res.send('Already Exist')
 					}
 				})
+				// if the image does not exist then save it to the database
 				.catch((error) => {
+					// creating a new URL object
 					const ur = new URL({
 						name: newVar,
 						url: base64Str,
 					})
+					// saving the object to the database
 					ur.save()
+					// generating a qr code from the url
 					QRcode.toDataURL(process.env.URL + `/image/${newVar}`, (er, ul) => {
 						if (!er) {
-							console.log(ul)
 							// rendering the qr code generated
 							res.render('qrcode', { url: ul })
 						} else {
@@ -72,6 +95,19 @@ router
 				})
 		}
 	})
+
+// creating the image route
+router.route('/image/:test').get((req, res) => {
+	// finding the image in the database
+	URL.findOne({ name: req.params.test })
+		.then((rl) => {
+			// rendering the image
+			res.render('image', { img: rl.url })
+		})
+		.catch((error) => {
+			res.send('Not Found')
+		})
+})
 
 // exporting the router
 module.exports = router
